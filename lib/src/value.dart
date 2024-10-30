045d5471060786a78968bc00591936faedaf284e
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-10-30 14:36:01 +0700

    Added value binding

--- lib/src/value.dart
+++ lib/src/value.dart
@@ -26,11 +26,94 @@ class _ValueChangeListener<T> extends ChangeListener {
   int get hashCode => listener.hashCode;
 }
 
+abstract class ValueBound<T> {
+  ValueListenable<T> get source;
+  ValueChangeNotifier<T> get target;
+
+  void assertDebugTargetChange();
+
+  void bind();
+  void unbind();
+}
+
+class UniDirectionalValueBound<T> extends ValueBound<T> {
+  final ValueListenable<T> source;
+  final ValueChangeNotifier<T> target;
+
+  UniDirectionalValueBound(this.source, this.target);
+
+  @override
+  void bind() {
+    target.value = source.value;
+    source.addListener(_listener);
+  }
+
+  void _listener() {
+    // This is to skip the target value change assertion
+    target._internalSetValue(source.value);
+  }
+
+  @override
+  void unbind() {
+    source.removeListener(_listener);
+  }
+
+  @override
+  void assertDebugTargetChange() {
+    throw AssertionError(
+        'Changing the target value of a unidirectional bound is prohibited');
+  }
+}
+
+class BiDirectionalValueBound<T> extends ValueBound<T> {
+  static const int _changeDirectionSource = 1;
+  static const int _changeDirectionTarget = 2;
+  final ValueNotifier<T> source;
+  final ValueChangeNotifier<T> target;
+
+  BiDirectionalValueBound(this.source, this.target);
+
+  int? _changeDirection;
+
+  @override
+  void bind() {
+    target.value = source.value;
+    source.addListener(_sourceListener);
+    target.addListener(_targetListener);
+  }
+
+  void _sourceListener() {
+    if (_changeDirection == _changeDirectionTarget) return;
+    _changeDirection = _changeDirectionSource;
+    target.value = source.value;
+    _changeDirection = null;
+  }
+
+  void _targetListener() {
+    if (_changeDirection == _changeDirectionSource) return;
+    _changeDirection = _changeDirectionTarget;
+    source.value = target.value;
+    _changeDirection = null;
+  }
+
+  @override
+  void unbind() {
+    source.removeListener(_sourceListener);
+    target.removeListener(_targetListener);
+  }
+
+  @override
+  void assertDebugTargetChange() {
+    // Do nothing
+  }
+}
+
 /// A value notifier class that notifies listeners of changes.
 class ValueChangeNotifier<T>
     with ChangesNotifierHelperMixin
-    implements ValueListenable<T> {
+    implements ValueNotifier<T> {
   T _value;
+  ValueBound<T>? _bound;
 
   /// Constructs a [ValueChangeNotifier] with an initial value.
   ValueChangeNotifier(this._value);
@@ -45,10 +128,45 @@ class ValueChangeNotifier<T>
     defaultRemoveListener(_ValueChangeListener(listener));
   }
 
+  bool get isBound => _bound != null;
+
+  ValueBound<T>? get bound => _bound;
+
+  void unbind() {
+    if (_bound == null) return;
+    _bound!.unbind();
+    _bound = null;
+  }
+
+  void uniDirectionalBind(ValueListenable<T> source) {
+    unbind();
+    var binding = UniDirectionalValueBound(source, this);
+    binding.bind();
+    _bound = binding;
+  }
+
+  void biDirectionalBind(ValueNotifier<T> source) {
+    unbind();
+    var binding = BiDirectionalValueBound(source, this);
+    binding.bind();
+    _bound = binding;
+  }
+
   @override
   T get value => _value;
 
+  @override
   set value(T newValue) {
+    assert(() {
+      if (_bound != null) {
+        _bound!.assertDebugTargetChange();
+      }
+      return true;
+    }());
+    _internalSetValue(newValue);
+  }
+
+  void _internalSetValue(T newValue) {
     if (_value == newValue) return;
     var previous = _value;
     _value = newValue;
@@ -68,5 +186,13 @@ class ValueChangeNotifier<T>
   @override
   String toString() {
     return 'ValueChangeNotifier(value: $_value)';
+  }
+
+  @override
+  @protected
+  @visibleForTesting
+  @pragma('vm:notify-debugger-on-exception')
+  void notifyListeners() {
+    throw UnsupportedError('Use defaultNotifyListeners instead');
   }
 }
